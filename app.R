5922810ffc09ed9d9911faee7d2ef931d760bbad
Tyelcie <tyelcie@gmail.com>
2018-07-14 00:23:27 +0800

    hide initial score

--- app.R
+++ app.R
@@ -1,53 +1,48 @@
 library(shiny)
+library(shinyjs)
 ui <- fluidPage(
-    
-    # Application title
+    useShinyjs(),
     titlePanel("Color Game"),
-    
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            p('Drag the sliders below to change your color pie,
-              and try to make it closeser to mine.
-              Then click the "My Score" button.'),
-            sliderInput("red","Red:",min = 0,max = 255,value = 0),
-            sliderInput("green","Green:",min = 0,max = 255,value = 0),
-            sliderInput("blue","Blue:",min = 0,max = 255,value = 0),
-            actionButton('go','My Score')
-        ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
+    mainPanel(
+        fluidRow(column(3,
+                        h4('Your Color'),
+                        plotOutput("distPlot")),
+                 column(3,
+                        h4('My Color'),
+                        plotOutput('samplePlot'))
+                 ),
             fluidRow(
-              column(3,
-                   h4('Your Color'),
-                   plotOutput("distPlot")),
-              column(3,
-                   h4('My Color'),
-                   plotOutput('samplePlot'))  
-            ),
+                column(2,sliderInput("red","Red:",min = 0,max = 255,value = 0)),
+                column(2,sliderInput("green","Green:",min = 0,max = 255,value = 0)),
+                column(2,sliderInput("blue","Blue:",min = 0,max = 255,value = 0))
+                ),
+            fluidRow(
+                column(3,actionButton('go','My Score')),
+                column(3,actionButton('refresh','Play Again'))
+                ),
             fluidRow(
                 column(6,
+                       p('Drag the sliders above to change your color pie,
+              and try to make it closeser to mine.
+              Then click the "My Score" button.'),
                        h3(textOutput('colordist')),
+                       hr(),
                        br("This is a simulator to FunGyan's game ",
                           strong('Guess Thy Color')," on Google Play Store."),
                        a('https://play.google.com/store/apps/details?id=com.fungyan.guessthycolor'),
                        br('The "percent match" calculation is based on Euclidian Distance, see:'),
                        a('https://en.wikipedia.org/wiki/Color_difference'))      
                        )
-                
             ))
-)
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output,session) {
     Range <- 256
     rR <- sample(0:255,1)
     rG <- sample(0:255,1)
     rB <- sample(0:255,1)
     output$samplePlot <- renderPlot({
         par(mar = c(0,0,0,0))
-        pie(2,col = rgb(rR/Range,rG/Range,rB/Range),
+        pie(1,col = rgb(rR/Range,rG/Range,rB/Range),
             labels = NA,lty = 0)
     })
     output$distPlot <- renderPlot({
@@ -55,16 +50,17 @@ server <- function(input, output) {
         G <- input$green
         B <- input$blue
         par(mar = c(0,0,0,0))
-        pie(2,col = rgb(R/Range,G/Range,B/Range),
+        pie(1,col = rgb(R/Range,G/Range,B/Range),
             labels = NA,lty = 0)
     })
-    output$colordist <- renderText({
-        input$go
-        dist <- isolate(1-(sqrt((input$red-rR)^2+(input$green-rG)^2+(input$blue-rB)^2)/sqrt((255^2)*3)))
+    observeEvent(input$go,{
+        output$colordist <- renderText({
+        dist <- 1-(sqrt((input$red-rR)^2+(input$green-rG)^2+(input$blue-rB)^2)/sqrt((255^2)*3))
         paste0('Percent Match: ',round(dist*100,0),'%')
+        })})
+    
+    observeEvent(input$refresh,{
+        session$reload()
     })
 }
-
-# Run the application 
 shinyApp(ui = ui, server = server)
-
